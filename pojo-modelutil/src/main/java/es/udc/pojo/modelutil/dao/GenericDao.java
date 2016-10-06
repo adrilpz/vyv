4fcf9b8584d5487954d448b6ef8b492056f460d0
adrian.lmartinez <adrian.lmartinez@udc.es>
2016-10-06 16:51:06 +0200

    Cuarto commit

---
+++ pojo-modelutil/src/main/java/es/udc/pojo/modelutil/dao/GenericDao.java
@@ -0,0 +1,15 @@
+package es.udc.pojo.modelutil.dao;
+
+import java.io.Serializable;
+
+import es.udc.pojo.modelutil.exceptions.InstanceNotFoundException;
+
+public interface GenericDao <E, PK extends Serializable>{
+
+	void save(E entity);
+
+	E find(PK id) throws InstanceNotFoundException;
+
+	void remove(PK id) throws InstanceNotFoundException;
+
+}
