05ac6f22256a83bf8929ab236277f4a9c1a6fb3f
adrian.lmartinez <adrian.lmartinez@udc.es>
2016-10-12 20:41:21 +0200

    eliminacion proyecto

--- src/main/java/es/udc/pa/p007/apuestasapp/web/pages/apuestas/CreatedTipoApuesta.java
+++
@@ -1,9 +0,0 @@
-package es.udc.pa.p007.apuestasapp.web.pages.apuestas;
-
-import es.udc.pa.p007.apuestasapp.web.services.AuthenticationPolicy;
-import es.udc.pa.p007.apuestasapp.web.services.AuthenticationPolicyType;
-
-@AuthenticationPolicy(AuthenticationPolicyType.ADMIN_USERS)
-public class CreatedTipoApuesta {
-
-}
--- src/main/java/es/udc/pa/p007/apuestasapp/web/services/AuthenticationPolicyType.java
+++
@@ -1,5 +0,0 @@
-package es.udc.pa.p007.apuestasapp.web.services;
-
-public enum AuthenticationPolicyType {
-	ALL_USERS, AUTHENTICATED_USERS, NON_AUTHENTICATED_USERS, ADMIN_USERS;
-}
